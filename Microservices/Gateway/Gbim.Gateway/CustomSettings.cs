2a04dfb778c33ac6b1b9a1a9382ebbd94fe4cac1
Raiymbek <40425204+MrAtkens@users.noreply.github.com>
2023-02-03 21:01:57 +0600

    Fly yml

--- Microservices/Gateway/Gbim.Gateway/CustomSettings.cs
+++
@@ -1,7 +0,0 @@
-﻿﻿namespace Gbim.Gateway {
-    /// <summary> Настройки сервиса </summary>
-    public class CustomSettings {
-        /// <summary> Включить трассировку запросов </summary>
-        public bool EnableTracing { get; set; }
-    }
-}
--- Microservices/Gateway/Gbim.Gateway/Settings/AccountApiOptions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Gbim.Gateway.Settings {
-    public class AccountApiOptions {
-        public string ApiEndpoint { get; set; }
-        public int TimeoutInSeconds { get; set; }
-    }
-}
